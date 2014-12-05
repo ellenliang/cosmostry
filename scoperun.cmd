821311c4a8300cf5875c13c22f97a5e0e279b0fa
Saveen Reddy <saveenr@live.com>
2014-12-05 13:40:41 -0800

    Moved SampleModules

--- scoperun.cmd
+++ scoperun.cmd
@@ -1,3 +1,3 @@
-c:\ScopeSDK\scope.exe run -i %1 %2 %3 %4 %5 %6 %7 %8 %9 -INPUT_PATH c:\github\CosmosSamples\VCROOT -OUTPUT_PATH c:\github\CosmosSamples\VCROOT -workingRoot c:\temp
+c:\ScopeSDK\scope.exe run -i %1 %2 %3 %4 %5 %6 %7 %8 %9 -INPUT_PATH c:\github\CosmosSamples\VCROOT -OUTPUT_PATH c:\github\CosmosSamples\VCROOT -RESOURCE_PATH c:\github\CosmosSamples\VCROOT -workingRoot c:\temp   
 
     
